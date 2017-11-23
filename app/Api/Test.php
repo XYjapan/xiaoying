fd380e1019eec226968aa3896e7ac02cfc427cde
Mrlaozhou <zgs5999@163.com>
2017-11-23 13:26:43 +0800

    add app@Common@helper.php & config@constant.php

---
+++ app/Api/Test.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Api;
+
+use Illuminate\Http\Request;
+use App\Api\Api;
+
+class Test extends Api
+{
+    public function index( Request $request )
+    {
+        //
+        
+    }
+}
---
+++ config/constant.php
@@ -0,0 +1,7 @@
+<?php
+
+// 路径常量
+
+defined('APP_PATH') ?: define( 'APP_PATH', '../app' );
+
+defined('COMMON_PATH') ?: define( 'COMMON_PATH', APP_PATH.'/Common' );
